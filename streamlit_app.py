407d27d5a5bb6e67a97f676b17a41e477abedd1b
NadyaEssebbar <nadyaessebbar@gmail.com>
2023-03-21 09:21:22 +0100

    Update streamlit_app.py
    load fruitlist form AWS s3 bucket
    display fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+
+#use panda to load the csv fron an AWS s3 bucket
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast favorites')
@@ -8,7 +13,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+streamlit.dataframe(my_fruit_list)
 
 
 
