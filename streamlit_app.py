b1ffa05df8d801c5dc9ae4033a638fdbacf5f122
NadyaEssebbar <nadyaessebbar@gmail.com>
2023-03-21 13:49:57 +0100

    Update streamlit_app.py
    Get all fruits and display in table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+rimport streamlit
 import pandas
 import requests
 import snowflake.connector
@@ -45,6 +45,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("select * from FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+#my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+# Displays the data into a table
+streamlit.dataframe(my_data_rows)
